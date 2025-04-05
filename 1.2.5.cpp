4892846375b8a87b40a62303ea2d1343ea6b0806
VOID-3560 <dev.bhardwaj.3560@gmail.com>
2025-04-05 10:47:12 +0530

    conflict removal

--- 1.2.5.cpp
+++ 1.2.5.cpp
@@ -1,56 +1,32 @@
 #include <iostream>
 
+double gettotal(double prices[]);
+// Function to calculate the total price of items in an array   
 
-int search(int num[], int size, int my_num)
+double gettotal(double prices[])
 {
-    // Function to search for an element in an array    
-    // The function takes an array, its size, and the number to search for
-    // as parameters.   
+    double total = 0.0;
 
-    // It returns the index of the element if found, or -1 if not found.
-
-    // The function uses a for loop to iterate over the array and compare 
-    // each element with the target number.  
-    for (int i = 0; i < size; i++)
+    // Using a for loop to iterate over the array and calculate the total price
+    // The loop runs 5 times, once for each element in the array.               
+        
+    for (int i = 0; i < 5; i++)
     {
-        if (num[i] == my_num)
-        {
-            return i;
-        }
+        total += prices[i];
     }
-    return -1; // not found
+    
+    return total;
 }
-
 
 int main()
 {
 
-    int num[] = {1, 2, 3, 4, 5};
-    int size = sizeof(num) / sizeof(num[0]);
+    double prices[] = {19.99, 29.99, 39.99, 49.99, 59.99};
 
-    int index;
-    int my_num;
+    double total = gettotal(prices);
+    std::cout << "Total: " << total << " $"<< '\n';
+    std::cout << '\n';
 
-    std::cout << "entrer element to search :";
-    std::cin >> my_num;
-
-    int searching = search(num , size , my_num);
-    
-    // The search function is called with the array, its size, and the 
-    // number to search for.    
-    
-    // The result is stored in the variable searching. 
-
-    if (searching != -1)// If the element is found, the index is printed.
-    {
-        std::cout << "Element found at index: " << searching << '\n';
-    }
-    else
-    // If the element is not found, a message is printed indicating 
-    // that the element was not found.
-    {
-        std::cout << "Element not found" << '\n';
-    }
 
     return 0;
 }
