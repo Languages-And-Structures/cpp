1a84b7d611dbdf3cb563c7bd4806ee0cbef6a12d
VOID-3560 <dev.bhardwaj.3560@gmail.com>
2025-04-06 22:03:55 +0530

    little little recursion

--- 1.3.6.cpp
+++ 1.3.6.cpp
@@ -18,3 +18,75 @@ int main() {
 
     return 0;
 }
+
+// EG 1
+
+// int countDigits(int 12345) {
+//     if (12345 == 0) return 0;
+//     return 1 +  |int countDigits(int 1234) {
+//                 |if (1234 == 0) return 0;
+//                 return 1 +  |int countDigits(int 123) {
+//                             |if (123 == 0) return 0;
+//                             return 1 +  |int countDigits(int 12) {
+//                                         |if (12 == 0) return 0;   
+//                                         return 1 +  |int countDigits(int 1) {
+//                                                     |if (1 == 0) return 0;
+//                                                     return 1 +  |int countDigits(int 0) {
+//                                                                 |if (n == 0) return 0;
+//                             |}
+//                 |} 
+    
+// }
+
+
+// countDigits(12345)
+// → 1 + countDigits(1234)
+//      → 1 + countDigits(123)
+//           → 1 + countDigits(12)
+//                → 1 + countDigits(1)
+//                     → 1 + countDigits(0)
+//                          → return 0 (base case hit)
+//                     → return 1 + 0 = 1
+//                → return 1 + 1 = 2
+//           → return 1 + 2 = 3
+//      → return 1 + 3 = 4
+// → return 1 + 4 = 5
+
+
+//EG 2
+
+// bool isPowerOfTwo(int 16) {
+//     if (16 == 1) return true;
+//     if (16 == 0 || n % 2 != 0) return false;
+//     return  |bool isPowerOfTwo(int 8) {
+//             |if (8 == 1) return true;
+//             |if (8 == 0 || n % 2 != 0) return false;
+//             |return  |bool isPowerOfTwo(int 4) {
+//                     |if (4 == 1) return true;
+//                     |if (4 == 0 || n % 2 != 0) return false;
+//                     |return  |bool isPowerOfTwo(int 2) {
+//                             |if (2 == 1) return true;
+//                             |if (2 == 0 || n % 2 != 0) return false;
+//                             |return  |bool isPowerOfTwo(int 1) {
+//                                     |if (1 == 1) return true;
+//                                     |if (1 == 0 || n % 2 != 0) return false;
+//                                     |return  isPowerOfTwo(0);
+//     }
+    
+// }
+
+// EG 3
+
+//int fib(int 6) {
+//     if (6 <= 1) return n;
+//     return fib(5) + fib(4);
+//         
+//fib(6)
+// → fib(5) + fib(4)
+// → fib(4) + fib(3) + fib(3) + fib(2)
+// → fib(3) + fib(2) + fib(2) + fib(1) + fib(2) + fib(1)
+// → fib(3) + fib(2) + fib(2) + 1 + fib(2) + 1
+// → fib(2) + fib(1) + fib(1) + fib(0) + fib(1) + fib(0) + 1 + 1
+// → fib(1) + fib(0) + 1 + 1 + 1 + 1 + 1 + 1
+// → 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+
