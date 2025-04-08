f4864d9627cadccf85dea753f6c7c51b1c08190f
VOID-3560 <dev.bhardwaj.3560@gmail.com>
2025-04-08 21:05:19 +0530

    more recursion

---
+++ 1.3.9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+void reverseString(std::string& str, int left, int right) {
+    if (left >= right) return;
+    std::swap(str[left], str[right]);
+    reverseString(str, left + 1, right - 1);
+}
+int main() {
+    std::string s = "hello";
+    reverseString(s, 0, s.size() - 1);
+    std::cout << "Reversed string: " << s << '\n';
+    return 0;
+}
