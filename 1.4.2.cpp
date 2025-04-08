2e7e3e6a542e14a50b49f370f8017b82077ffc84
VOID-3560 <dev.bhardwaj.3560@gmail.com>
2025-04-08 21:06:35 +0530

    more recursion

---
+++ 1.4.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int multiply(int a, int b) {
+    if (b < 0) return -multiply(a, -b);
+    if (b == 0) return 0;
+    return a + multiply(a, b - 1);
+}
+int main() {
+    int a = 4, b = 3;
+    std::cout << "Product: " << multiply(a, b) << '\n';
+    return 0;
+}
